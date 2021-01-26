98fd99f79fa0891d5556e3255d427da707613221
jbataille-git <jozefbee@gmail.com>
2021-01-26 17:01:07 +0100

    Eerste commit van Winc-werkdirectory vanaf level 18

---
+++ 18_js_array/level_18_01_arrow/opdracht.js
@@ -0,0 +1,39 @@
+// zet de functie om naar een arrow functie
+const ikRockArrowFunctions = function () {
+  console.log("Joe, ik rock de arrow functions!");
+};
+
+ikRockArrowFunctions();
+
+let ikRock = () => {
+    console.log("Joe, ik rock de arrow functions");
+}
+
+// Deel A
+let ikRock = () => console.log("Joe, ik rock de arrow functions");
+
+const fivePlusSeven = function () {
+  return 5 + 7;
+};
+
+fivePlusSeven();
+
+// als de functie-body niet op q regel kan, heb je een return statement nodig
+let fivePlusSeven3 = () => {
+  return 5 + 7;
+};
+
+// Deel B
+// hier een impliciet return-statement
+let fivePlusSeven2 = () => 5 + 7;
+
+// Deel C
+let myFunction = (a, b) => a + b;
+
+// Deel D
+let addFive = c => c + 5;
+
+// Deel E
+let createObject = () => ({greeting: "hoi"});
+
+createObject();
